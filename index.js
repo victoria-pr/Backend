6dd2dfaf75240e02b8639a0f13e5b22238136080
Vicky <Vicky@MacBook-Pro-de-Dalila.local>
2023-03-29 15:51:15 +0200

    PlayerAPIController hecho

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
-import router from "./routes/router.js";
+import routerAPI from "./routes/api/router.js";
 
 const app = express();
 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
 
+app.set("view engine", "pug");
 
-app.use ("/", router);
+
+app.use ("/api", routerAPI);
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000");
